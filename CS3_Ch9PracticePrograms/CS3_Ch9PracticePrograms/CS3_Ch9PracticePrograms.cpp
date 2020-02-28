7ee9924c77950b2706e8e2ea64c251e60352606d
AndrewLam97 <alam1997@gmail.com>
2020-02-27 16:49:51 -0800

    Update CS3_Ch9PracticePrograms.cpp

--- CS3_Ch9PracticePrograms/CS3_Ch9PracticePrograms/CS3_Ch9PracticePrograms.cpp
+++ CS3_Ch9PracticePrograms/CS3_Ch9PracticePrograms/CS3_Ch9PracticePrograms.cpp
@@ -7,24 +7,38 @@ bool isPalindrome(char* cstr);
 
 int main()
 {
+    std::cout << "Problem #1" << std::endl;
     int problem1 = 5;
     int* ptr1 = &problem1;
-    std::cout << "Problem 1 initial value: " << problem1 << std::endl;
+    std::cout << "Initial value: " << problem1 << std::endl;
     addOne(ptr1);
-    std::cout << "Problem 1 added value: " << problem1 << std::endl;
+    std::cout << "Added value: " << problem1 << std::endl;
 
+    std::cout << std::endl << "Problem #2" << std::endl;
     arrayAvg();
 
+    std::cout << std::endl << "Problem #3" << std::endl;
     char s1[50] = "neveroddoreven";
     char s2[50] = "not a palindrome";
     std::cout << isPalindrome(s1) << std::endl; // true
     std::cout << isPalindrome(s2) << std::endl; // false
 }
-
+/*
+PURPOSE: increments the referenced integer by one
+PARAMETERS: integer pointer
+RETURN VALUES: none
+*/
 void addOne(int* ptrNum) {
     *ptrNum += 1;
 }
 
+/*
+PURPOSE: Create a dynamic array that can store numDoubles doubles and a loop that allows the user to fill
+the dynamic array with values. Loops through the array and calculates then outputs the average.
+Deletes the dynamic array before exiting.
+PARAMETERS: none, takes value from console during runtime
+RETURN VALUES: none, outputs result to console
+*/
 void arrayAvg() {
     double numDoubles;
     std::cout << "Enter size of dynamic array: " << std::endl;
@@ -46,6 +60,11 @@ void arrayAvg() {
     delete[] doubleArr;
 }
 
+/*
+PURPOSE: Checks if cstr is a palindrome
+PARAMETERS: char pointer to cstring
+RETURN VALUES: boolean of palindrome status
+*/
 bool isPalindrome(char* cstr){
     char* front = cstr;
     char* back = cstr + strlen(cstr) - 1;
